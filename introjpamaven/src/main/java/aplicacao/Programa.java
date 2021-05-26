4993334ad19a46407945e0b8b126ee0b9ec35ab9
Roger <logroger@gmail.com>
2021-05-25 23:30:48 -0300

    jpa mysql pessoa

--- introjpamaven/src/main/java/aplicacao/Programa.java
+++ introjpamaven/src/main/java/aplicacao/Programa.java
@@ -1,18 +1,30 @@
 package aplicacao;
 
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Jose da Silva", "jose@gmail.com");
-		Pessoa p2 = new Pessoa(1, "Pedro de Paula", "pedro@gmail.com");
-		Pessoa p3 = new Pessoa(1, "Joao Maria", "joaomaria@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Jose da Silva", "jose@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Pedro de Paula", "pedro@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Joao Maria", "joaomaria@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Ok dba... rr");
+				
+		
 	}
 
 }
