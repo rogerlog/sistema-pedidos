2258451577ecfb3b603dfc43df9ec7b97c02000b
Roger <logroger@gmail.com>
2021-05-25 23:50:39 -0300

    first commit sispedidos

---
+++ sispedidos/src/main/java/io/github/rogerlog/SispedidosApplication.java
@@ -0,0 +1,13 @@
+package io.github.rogerlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SispedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SispedidosApplication.class, args);
+	}
+
+}
