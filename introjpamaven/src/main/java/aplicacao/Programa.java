3d078ee03c46587f920fd3889fb660436b2d6103
Roger <logroger@gmail.com>
2021-05-25 23:34:19 -0300

    find by pessoa id

--- introjpamaven/src/main/java/aplicacao/Programa.java
+++ introjpamaven/src/main/java/aplicacao/Programa.java
@@ -10,20 +10,29 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		
+		/*
 		Pessoa p1 = new Pessoa(null, "Jose da Silva", "jose@gmail.com");
 		Pessoa p2 = new Pessoa(null, "Pedro de Paula", "pedro@gmail.com");
 		Pessoa p3 = new Pessoa(null, "Joao Maria", "joaomaria@gmail.com");
+		*/
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
+		/*
 		em.getTransaction().begin();
 		em.persist(p1);
 		em.persist(p2);
 		em.persist(p3);
 		em.getTransaction().commit();
+		*/
+		
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		System.out.println(p);
+		
 		System.out.println("Ok dba... rr");
-				
+		em.close();
+		emf.close();
 		
 	}
 
