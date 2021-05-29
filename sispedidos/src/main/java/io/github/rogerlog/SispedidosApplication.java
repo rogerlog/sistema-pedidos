f4c2ffc3fed292fbc3e75ea08436c3b287635fc8
Roger <logroger@gmail.com>
2021-05-29 11:58:15 -0300

    Estado e Cidade

--- sispedidos/src/main/java/io/github/rogerlog/SispedidosApplication.java
+++ sispedidos/src/main/java/io/github/rogerlog/SispedidosApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import io.github.rogerlog.domain.Categoria;
+import io.github.rogerlog.domain.Cidade;
+import io.github.rogerlog.domain.Estado;
 import io.github.rogerlog.domain.Produto;
 import io.github.rogerlog.repositories.CategoriaRepository;
+import io.github.rogerlog.repositories.CidadeRepository;
+import io.github.rogerlog.repositories.EstadoRepository;
 import io.github.rogerlog.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,13 @@ public class SispedidosApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;	
+	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SispedidosApplication.class, args);
@@ -42,9 +53,23 @@ public class SispedidosApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
 		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+					
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2 ,p3));
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 		
 	}
 }
