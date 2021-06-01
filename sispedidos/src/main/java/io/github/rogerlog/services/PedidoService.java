843059518984e7a61e9e5934f63196524315af01
Roger <logroger@gmail.com>
2021-06-01 08:56:17 -0300

    PedidoService find id

---
+++ sispedidos/src/main/java/io/github/rogerlog/services/PedidoService.java
@@ -0,0 +1,23 @@
+package io.github.rogerlog.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import io.github.rogerlog.domain.Pedido;
+import io.github.rogerlog.repositories.PedidoRepository;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+
+		return obj.orElseThrow(() -> new io.github.rogerlog.services.exceptions.ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
