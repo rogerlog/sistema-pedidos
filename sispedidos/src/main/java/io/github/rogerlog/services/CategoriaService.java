d906f60dc4fb56162dff529bbe47d47a3c575a37
Roger <logroger@gmail.com>
2021-06-03 20:07:50 -0300

    CategoriaDTO  insert

--- sispedidos/src/main/java/io/github/rogerlog/services/CategoriaService.java
+++ sispedidos/src/main/java/io/github/rogerlog/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import io.github.rogerlog.CategoriaDTO;
 import io.github.rogerlog.domain.Categoria;
 import io.github.rogerlog.repositories.CategoriaRepository;
 import io.github.rogerlog.services.exceptions.DataIntegrityException;
@@ -52,6 +53,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
