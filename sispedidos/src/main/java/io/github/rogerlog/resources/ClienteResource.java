a188f495cdf73429b4464dd04fb24976d491e8a3
Roger <logroger@gmail.com>
2021-06-04 23:50:01 -0300

    ClienteResource, POST insert fromDTO

--- sispedidos/src/main/java/io/github/rogerlog/resources/ClienteResource.java
+++ sispedidos/src/main/java/io/github/rogerlog/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package io.github.rogerlog.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import io.github.rogerlog.domain.Cliente;
 import io.github.rogerlog.dto.ClienteDTO;
+import io.github.rogerlog.dto.ClienteNewDTO;
 import io.github.rogerlog.services.ClienteService;
 
 @RestController
@@ -32,6 +35,15 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}	
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
