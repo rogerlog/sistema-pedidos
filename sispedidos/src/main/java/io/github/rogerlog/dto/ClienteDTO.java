961417aeef19474c211d5d159e3d3199f581172b
Roger <logroger@gmail.com>
2021-06-05 00:21:37 -0300

    Cliente update annotation

--- sispedidos/src/main/java/io/github/rogerlog/dto/ClienteDTO.java
+++ sispedidos/src/main/java/io/github/rogerlog/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import io.github.rogerlog.domain.Cliente;
+import io.github.rogerlog.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
