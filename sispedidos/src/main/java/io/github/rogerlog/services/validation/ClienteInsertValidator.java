90fdfe34a04dd9084b3088df39096fed884d0096
Roger <logroger@gmail.com>
2021-06-05 00:16:29 -0300

    Validacao de unicidade de email de cliente

--- sispedidos/src/main/java/io/github/rogerlog/services/validation/ClienteInsertValidator.java
+++ sispedidos/src/main/java/io/github/rogerlog/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import io.github.rogerlog.domain.Cliente;
 import io.github.rogerlog.domain.enums.TipoCliente;
 import io.github.rogerlog.dto.ClienteNewDTO;
+import io.github.rogerlog.repositories.ClienteRepository;
 import io.github.rogerlog.resources.exception.FieldMessage;
 import io.github.rogerlog.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -30,6 +37,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}		
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
