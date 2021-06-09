b926327a3b8f06e27bbf0a5747122de6cf87a58c
Roger <logroger@gmail.com>
2021-06-08 23:37:20 -0300

    DBService insert data

--- sispedidos/src/main/java/io/github/rogerlog/config/TestConfig.java
+++ sispedidos/src/main/java/io/github/rogerlog/config/TestConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import io.github.rogerlog.services.DBService;
+
 @Configuration
 @Profile("test")
 public class TestConfig {
