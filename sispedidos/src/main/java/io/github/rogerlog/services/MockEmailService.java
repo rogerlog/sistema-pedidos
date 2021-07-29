019efea7e319317907be34dacf1505c68d46e305
Roger <logroger@gmail.com>
2021-07-29 13:11:23 -0300

    Abstract Class to mail service

--- sispedidos/src/main/java/io/github/rogerlog/services/MockEmailService.java
+++ sispedidos/src/main/java/io/github/rogerlog/services/MockEmailService.java
@@ -1,5 +1,17 @@
 package io.github.rogerlog.services;
 
-public class MockEmailService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.SimpleMailMessage;
 
+public class MockEmailService extends AbstractEmailService {
+
+	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
+
+	@Override
+	public void sendEmail(SimpleMailMessage msg) {
+		LOG.info("Simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+	}
 }
